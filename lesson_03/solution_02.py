fcba9fba472357097fabe5049d2c7170fe293948
Mikalai Simak <kilzil16@gmail.com>
2022-05-22 15:13:36 +0300

    Add solution

---
+++ lesson_03/solution_02.py
@@ -0,0 +1,17 @@
+In [1]: vklad = 2130
+
+In [2]: bonus = 120
+
+In [3]: god1 = vklad*1.1 + bonus
+
+In [4]: god2=god1*1.1 + bonus
+
+In [5]: god3=god2*1.1 + bonus
+
+In [6]: god4=god3*1.1 + bonus
+
+In [7]: god5=god4*1.1 + bonus
+
+In [8]: god5
+Out[8]: 4162.998300000001
+
---
+++ lesson_03/solution_03.py
@@ -0,0 +1,7 @@
+In [1]: my_list = [1, "b", "y", 5, "g"]
+
+In [2]: my_str=str(my_list)
+
+In [3]: print(my_str[::-1])
+]'g' ,5 ,'y' ,'b' ,1[
+
